b8e2705046e16ce7753e39cc253e9e29cd0b5544
arslan1119 <tagandurdyyevarslan77@gmail.com>
2026-09-08 02:56:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,7 +111,9 @@ def analyze_market(symbol="EURUSD=X"):
 
     except Exception as e:
          return f"❌ Error: {str(e)}"
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update,
+context:
+ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
 
     text = f"""
