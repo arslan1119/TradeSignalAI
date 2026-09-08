b8d8b5c1fdc91d4fc5378bc9619814536d875c17
arslan1119 <tagandurdyyevarslan77@gmail.com>
2026-09-09 01:28:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -174,10 +174,10 @@ async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if message == "📈 Live Signals":
         analysis = find_best_signal()
 
-    await update.message.reply_text(
-        analysis,
-        parse_mode="HTML"
-    )
+        await update.message.reply_text(
+            analysis,
+            parse_mode="HTML"
+        )
 
     elif message == "📊 Market Analytics":
         await update.message.reply_text(
